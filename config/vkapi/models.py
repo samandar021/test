7271ea85c582eaccf5262711f0b8aa5872dba6a5
Samandar Panjiev <panjiyevs021@gmail.com>
2023-08-17 18:28:01 +0500

    Query class models for APIs

--- config/vkapi/models.py
+++ config/vkapi/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    profile_id = models.CharField(max_length=255)
+    avatar_url = models.URLField()
+    followers = models.PositiveIntegerField()
+    following = models.PositiveIntegerField()
+
+
+class Post(models.Model):
+    post_id = models.CharField(max_length=255)
+    url = models.URLField()
+    likes = models.PositiveIntegerField()
+    share = models.PositiveIntegerField()
+    views = models.PositiveIntegerField()
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    # Add other fields as needed
