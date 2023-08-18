871ce86189def830743de103f9b0c54e3a4777aa
Samandar Panjiev <panjiyevs021@gmail.com>
2023-08-18 11:17:07 +0500

    Views are updated

--- config/vkapi/urls.py
+++ config/vkapi/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import ProfileView, LikesView, PostsListView
+from .views import *
 
 urlpatterns = [
     path('v1/profile/', ProfileView.as_view(), name='profile'),
     path('v1/likes/', LikesView.as_view(), name='likes'),
     path('v1/posts/', PostsListView.as_view(), name='Last 10 posts with one post like and shares'),
-    path('profile/', ProfileView.as_view(), name='profile-view'),
-    path('likes/', LikesView.as_view(), name='likes-view'),
+    path('profile/', ProfileViewD.as_view(), name='profile-view'),
+    path('likes/', LikesViewD.as_view(), name='likes-view'),
 
 
 ]
